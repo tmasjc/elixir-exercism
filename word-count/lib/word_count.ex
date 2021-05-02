63035cf2492c351c68c91b43b5e01ed66b0a2ab0
tmasjc <thomas.jcng@gmail.com>
2021-05-03 00:16:12 +0800

    Enumerables - map & reduce

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -4,7 +4,17 @@ defmodule WordCount do
 
   Words are compared case-insensitively.
   """
+
   @spec count(String.t()) :: map
   def count(sentence) do
+    strs = sentence
+      |> String.downcase()
+      |> String.replace(~r/@|#|\$|%|&|\^|:|_|!|,/u, " ")
+      |> String.split(" ", trim: true)
+    Enum.reduce(strs, Map.new(), &count_this/2)
+  end
+
+  defp count_this(word, arr) do
+    Map.update(arr, word, 1, &(&1 + 1))
   end
 end
