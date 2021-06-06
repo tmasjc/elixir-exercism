4a02049f7ba4f4dcdc9b5d2cc5d1c5e03996413a
tmasjc <thomas.jcng@gmail.com>
2021-06-06 19:11:10 +0800

    Convert number to Roman numerals
    using module attributes and recursion

---
+++ roman-numerals/lib/roman_numerals.ex
@@ -0,0 +1,34 @@
+defmodule RomanNumerals do
+  @romana %{
+    1    => "I",
+    4    => "IV",
+    5    => "V",
+    9    => "IX",
+    10   => "X",
+    40   => "XL",
+    50   => "L",
+    90   => "XC",
+    100  => "C",
+    400  => "CD",
+    500  => "D",
+    900  => "CM",
+    1000 => "M"
+  }
+  @keys Map.keys(@romana) |> Enum.sort(&(&1 > &2))
+  @doc """
+  Convert the number to a roman number.
+  """
+  @spec numeral(pos_integer) :: String.t()
+  def numeral(number) do
+    to_roman(@keys, number, "")
+  end
+
+  defp to_roman(_, 0, result), do: result
+  defp to_roman([head | rest] = keys, number, result) do
+    if number >= head do
+      to_roman(keys, number - head, result <> @romana[head])
+    else
+      to_roman(rest, number, result)
+    end
+  end
+end
