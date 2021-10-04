b852e5d7336bf0f76ddb989ac1330b1f72b6615e
tmasjc <thomas.jcng@gmail.com>
2021-10-04 23:33:27 +0800

    Refactor to idiomatic code

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -1,9 +1,21 @@
 defmodule RnaTranscription do
+  # module attribute
   @guanine "G"
   @adenine "A"
   @cystosine "C"
   @uracil "U"
   @thymine "T"
+
+  # define a private function
+  defp transcribe_dna(nucleotide) do
+    case nucleotide do
+      @guanine -> @cystosine
+      @cystosine -> @guanine
+      @thymine -> @adenine
+      @adenine -> @uracil
+      _ -> "Error"
+    end
+  end
 
   @doc """
   Transcribes a character list representing DNA nucleotides to RNA
@@ -15,17 +27,11 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    s = String.split(to_string(dna), "", trim: true)
-    rna = Enum.map(s, fn(x) ->
-      case x do
-        @guanine -> @cystosine
-        @cystosine -> @guanine
-        @thymine -> @adenine
-        @adenine -> @uracil
-        _ -> "Error"
-      end
-    end)
-    rna
+    dna
+    ## '' represents List, "" represents BitString
+    |> to_string
+    |> String.split("", trim: true)
+    |> Enum.map(&transcribe_dna/1)
     |> to_string
     |> to_charlist
   end
