578f0aa205c155437b2a33536a46026957c0d3cd
tmasjc <thomas.jcng@gmail.com>
2021-10-05 23:42:54 +0800

    Done func

--- anagram/lib/anagram.ex
+++ anagram/lib/anagram.ex
@@ -4,15 +4,20 @@ defmodule Anagram do
   """
 
   @doc """
-  Hello world.
+  Check if two strings are Anagrams.
 
-  ## Examples
-
-      iex> Anagram.hello()
-      :world
-
+  A and B are considered Anagrams if A can be rearrange to B.
   """
-  def hello do
-    :world
+  @spec is_anagram?(String.t(), String.t()) :: boolean()
+  def is_anagram?(a, b) when is_binary(a) and is_binary(b) do
+    sort_string(a) == sort_string(b)
   end
+
+  defp sort_string(string) do
+    string
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.sort()
+  end
+
 end
