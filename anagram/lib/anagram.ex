89130a960d1af70816d02312a2b69eec2ba4a4ee
tmasjc <thomas.jcng@gmail.com>
2021-10-05 23:13:27 +0800

    Initiate simple exercise
    try out mix and string funcs

---
+++ anagram/lib/anagram.ex
@@ -0,0 +1,18 @@
+defmodule Anagram do
+  @moduledoc """
+  Documentation for `Anagram`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Anagram.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
