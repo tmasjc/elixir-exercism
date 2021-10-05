4fab74ddca6ef54dc59d7d636652d553efbcb4cc
tmasjc <thomas.jcng@gmail.com>
2021-10-05 23:59:32 +0800

    Provide test case

--- anagram/test/anagram_test.exs
+++ anagram/test/anagram_test.exs
@@ -2,7 +2,11 @@ defmodule AnagramTest do
   use ExUnit.Case
   doctest Anagram
 
-  test "greets the world" do
-    assert Anagram.hello() == :world
+  test "this works" do
+    assert Anagram.is_anagram?("abc", "CBA") == true
+  end
+
+  assert_raise FunctionClauseError, fn ->
+    Anagram.is_anagram?("abc", :abc) == true
   end
 end
