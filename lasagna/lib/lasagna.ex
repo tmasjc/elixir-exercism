c0b93dc9622153508ad4e0558ef0d3f03ef85b25
tmasjc <thomas.jcng@gmail.com>
2021-11-01 22:35:15 +0800

    Complete exercise.

--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -1,11 +1,8 @@
 defmodule Lasagna do
-  # TODO: define the 'expected_minutes_in_oven/0' function
-
-  # TODO: define the 'remaining_minutes_in_oven/1' function
-
-  # TODO: define the 'preparation_time_in_minutes/1' function
-
-  # TODO: define the 'total_time_in_minutes/2' function
-
-  # TODO: define the 'alarm/0' function
+  @fullminutes 40
+  def expected_minutes_in_oven(), do: @fullminutes
+  def remaining_minutes_in_oven(mins), do: @fullminutes - mins
+  def preparation_time_in_minutes(layers), do: layers * 2
+  def total_time_in_minutes(layers, mins), do: mins + preparation_time_in_minutes(layers)
+  def alarm(), do:  "Ding!"
 end
