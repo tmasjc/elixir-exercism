e155d638b0831d8ecccab96200cc53916452ba7d
tmasjc <thomas.jcng@gmail.com>
2021-11-07 16:04:08 +0800

    Complete Bob

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,5 +1,24 @@
 defmodule Bob do
   @spec hey(String.t()) :: String.t()
   def hey(input) do
+    cond do
+      is_blank?(input) -> "Fine. Be that way!"
+      is_question?(input) and is_shouting?(input) -> "Calm down, I know what I'm doing!"
+      is_question?(input) -> "Sure."
+      is_shouting?(input) -> "Whoa, chill out!"
+      true -> "Whatever."
+    end
+  end
+
+  defp is_blank?(input) do
+    String.trim(input) == ""
+  end
+
+  defp is_question?(input) do
+    input |> String.trim() |> String.ends_with?("?")
+  end
+
+  defp is_shouting?(input) do
+    input == String.upcase(input) and input != String.downcase(input)
   end
 end
