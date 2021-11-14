0629569d2d54363421b4dda7dc5637234bc889de
tmasjc <thomas.jcng@gmail.com>
2021-11-14 13:53:11 +0800

    Complete exercise

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,28 @@
 defmodule FreelancerRates do
+  @multiplier 8.0
+  @billable 22
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * @multiplier
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * (1 - discount / 100.0)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    hourly_rate
+    |> daily_rate()
+    |> apply_discount(discount)
+    |> (&(&1 * @billable)).()
+    |> Float.ceil()
+    |> Kernel.trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    hourly_rate
+    |> daily_rate()
+    |> apply_discount(discount)
+    |> (&(budget / &1)).()
+    |> Float.floor(1)
   end
 end
