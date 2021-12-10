241304ddfc8d0e9425c343115bab1e935fc30587
tmasjc <thomas.jcng@gmail.com>
2021-12-10 18:52:15 +0800

    Submit solution

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,9 +1,24 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 and !legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and !legacy? -> :fatal
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    x = to_label(level, legacy?)
+    case x do
+      :error -> :ops
+      :fatal -> :ops
+      :unknown when legacy? -> :dev1
+      :unknown when not legacy? -> :dev2
+      _ -> false
+    end
   end
 end
