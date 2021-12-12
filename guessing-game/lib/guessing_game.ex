0494546fbb2cfe9c8f5c482b7b9b034a64cdc7ef
tmasjc <thomas.jcng@gmail.com>
2021-12-12 09:52:52 +0800

    Passed all tests

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,5 +1,17 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
+  def compare(secret_number, guess \\ :no_guess)
+
+  def compare(secret_number, guess) when is_integer(guess) do
+    diff = guess - secret_number
+    cond do
+      diff == 0 -> "Correct"
+      abs(diff) == 1 -> "So close"
+      diff > 1 -> "Too high"
+      diff < 1 -> "Too low"
+    end
+  end
+
+  def compare(_secret_number, guess) when is_atom(guess) do
+    "Make a guess"
   end
 end
