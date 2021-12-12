718af06a2bbc95718fe1a3b77815ed530388d464
tmasjc <thomas.jcng@gmail.com>
2021-12-12 09:59:20 +0800

    Update solution
    remove any control structure

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,17 +1,8 @@
 defmodule GuessingGame do
   def compare(secret_number, guess \\ :no_guess)
-
-  def compare(secret_number, guess) when is_integer(guess) do
-    diff = guess - secret_number
-    cond do
-      diff == 0 -> "Correct"
-      abs(diff) == 1 -> "So close"
-      diff > 1 -> "Too high"
-      diff < 1 -> "Too low"
-    end
-  end
-
-  def compare(_secret_number, guess) when is_atom(guess) do
-    "Make a guess"
-  end
+  def compare(secret_number, secret_number) do "Correct" end
+  def compare(_, guess) when is_atom(guess) do "Make a guess" end
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1 do "So close" end
+  def compare(secret_number, guess) when guess > secret_number do "Too high" end
+  def compare(secret_number, guess) when guess < secret_number do "Too low" end
 end
