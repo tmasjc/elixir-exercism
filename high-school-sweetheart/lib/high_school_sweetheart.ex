5d7e426c9a8455f5d78359f7a7bf19fd1fcf4794
tmasjc <thomas.jcng@gmail.com>
2021-12-25 17:20:27 +0800

    Done exercise

--- high-school-sweetheart/lib/high_school_sweetheart.ex
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,35 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    name |> String.trim() |> String.first()
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    name |> first_letter() |> String.upcase() |> (&(&1 <> ".")).()
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    [f , l] = full_name |> String.split(" ") |> Enum.map(fn x -> initial(x) end)
+    f <> " " <> l
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
-
-    # Please implement the pair/2 function
+    tmp =
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     X. X.  +  X. X.     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+    String.replace(tmp, "X. X.  +  X. X.", initials(full_name1) <> "  +  " <> initials(full_name2), global: true)
   end
 end
