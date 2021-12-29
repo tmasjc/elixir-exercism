a5dae6fe2cd8bb03ae5b88bb73c54184ee40e8f2
tmasjc <thomas.jcng@gmail.com>
2021-12-30 00:22:54 +0800

    Done exercise
    charlist

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,10 +1,20 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
 
-    # Please implement the sanitize/1 function
+  @spec sanitize(charlist()) :: charlist()
+  def sanitize(username), do: sanitize(username, [])
+
+  defp sanitize([], acc), do: acc
+
+  defp sanitize([head | tail], acc), do: sanitize(tail, acc ++ transform_char(head))
+
+  defp transform_char(char) do
+    case char do
+      ?ä -> 'ae'
+      ?ö -> 'oe'
+      ?ü -> 'ue'
+      ?ß -> 'ss'
+      c when (c >= ?a and c <= ?z) or c == ?_ -> [c]
+      _ -> []
+    end
   end
 end
