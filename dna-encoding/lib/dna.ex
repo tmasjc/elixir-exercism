2d431ac1f03477a1a97a52044d0637af8fe973e6
tmasjc <thomas.jcng@gmail.com>
2022-01-16 12:19:38 +0800

    Done exercise
    working w/ bitstrings

--- dna-encoding/lib/dna.ex
+++ dna-encoding/lib/dna.ex
@@ -1,17 +1,28 @@
 defmodule DNA do
+
   def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+    case code_point do
+      ?\s -> 0b0000
+      ?A  -> 0b0001
+      ?C  -> 0b0010
+      ?G  -> 0b0100
+      ?T  -> 0b1000
+    end
   end
 
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case encoded_code do
+      0b0000 -> ?\s
+      0b0001 -> ?A
+      0b0010 -> ?C
+      0b0100 -> ?G
+      0b1000 -> ?T
+    end
   end
 
-  def encode(dna) do
-    # Please implement the encode/1 function
-  end
+  def encode([head | []]), do: <<encode_nucleotide(head)::4>>
+  def encode([head | tail]), do: <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
 
-  def decode(dna) do
-    # Please implement the decode/1 function
-  end
+  def decode(<<head::4>>), do: [decode_nucleotide(head)]
+  def decode(<<head::4, tail::bitstring>>), do: [decode_nucleotide(head) | decode(tail)]
 end
