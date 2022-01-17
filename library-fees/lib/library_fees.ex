7cca37e2970b6c894e0d60cc17f94acb76f0a01a
tmasjc <thomas.jcng@gmail.com>
2022-01-18 00:38:10 +0800

    Done exercise
    datetime in elixir

--- library-fees/lib/library_fees.ex
+++ library-fees/lib/library_fees.ex
@@ -1,25 +1,31 @@
 defmodule LibraryFees do
-  def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
-  end
+  def datetime_from_string(string), do: NaiveDateTime.from_iso8601!(string)
 
-  def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
-  end
+  def before_noon?(datetime), do: NaiveDateTime.to_time(datetime).hour < 12
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+
+    nday = if(before_noon?(checkout_datetime), do: 28, else: 29)
+
+    checkout_datetime
+    |> NaiveDateTime.add(nday * 24 * 3600, :second)
+    |> NaiveDateTime.to_date()
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    late = Date.diff(NaiveDateTime.to_date(actual_return_datetime), planned_return_date)
+    max(0, late)
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    Date.day_of_week(datetime) == 1
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    checkout = datetime_from_string(checkout)
+    return   = datetime_from_string(return)
+    fee      = days_late(return_date(checkout), return) * rate
+    actl_fee = if(monday?(return), do: fee * 0.5, else: fee)
+    trunc(actl_fee)
   end
 end
