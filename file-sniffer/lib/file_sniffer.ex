494d6aa1cf51ef4330d01164170cc8bc02e82b79
tmasjc <thomas.jcng@gmail.com>
2022-01-25 19:02:31 +0800

    Done exercise
    working with binaries

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -12,12 +12,18 @@ defmodule FileSniffer do
 
   def type_from_binary(file_binary) do
     case file_binary do
-      x when x in [<<0x7F>>, <<0x45>>, <<0x4c>>, <<0x46>>] -> type_from_extension("exe")
-      x when x in [<<0x42>>, <<0x4D>>] -> type_from_extension("bmp")
+      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> type_from_extension("exe")
+      <<0x42, 0x4D, _::binary>> -> type_from_extension("bmp")
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> type_from_extension("png")
+      <<0xFF, 0xD8, 0xFF, _::binary>> -> type_from_extension("jpg")
+      <<0x47, 0x49, 0x46, _::binary>> -> type_from_extension("gif")
     end
   end
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    case {type_from_binary(file_binary), type_from_extension(extension)} do
+      {type, type} -> {:ok, type}
+      _ -> {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
