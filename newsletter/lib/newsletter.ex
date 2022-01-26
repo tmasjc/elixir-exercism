37adf7ef96779239ec29e120c01bc393fea40eee
tmasjc <thomas.jcng@gmail.com>
2022-01-26 10:11:52 +0800

    Done new exercise

---
+++ newsletter/lib/newsletter.ex
@@ -0,0 +1,26 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
+  end
+
+  def open_log(path) do
+    File.open!(path, [:write])
+  end
+
+  def log_sent_email(pid, email) do
+    IO.puts(pid, email)
+  end
+
+  def close_log(pid) do
+    File.close(pid)
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    emails = read_emails(emails_path)
+    log    = open_log(log_path)
+    Enum.map(emails, fn x -> if send_fun.(x) == :ok, do: log_sent_email(log, x), else: false end)
+    close_log(log)
+  end
+end
