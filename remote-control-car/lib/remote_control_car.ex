04c64091babfe51cd93737355618b3c284ca62fb
tmasjc <thomas.jcng@gmail.com>
2022-01-28 22:59:18 +0800

    WIP

---
+++ remote-control-car/lib/remote_control_car.ex
@@ -0,0 +1,28 @@
+defmodule RemoteControlCar do
+  @enforce_keys [:nickname]
+  defstruct [
+    :nickname,
+    battery_percentage: 100,
+    distance_driven_in_meters: 0
+  ]
+
+  def new() do
+    %RemoteControlCar{nickname: "none"}
+  end
+
+  def new(nickname) do
+    %RemoteControlCar{nickname: nickname}
+  end
+
+  def display_distance(remote_car) do
+    # Please implement the display_distance/1 function
+  end
+
+  def display_battery(remote_car) do
+    # Please implement the display_battery/1 function
+  end
+
+  def drive(remote_car) do
+    # Please implement the drive/1 function
+  end
+end
