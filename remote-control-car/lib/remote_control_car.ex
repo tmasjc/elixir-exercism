8d483a4ce968a243bda44bf99d389ecd16b1473e
tmasjc <thomas.jcng@gmail.com>
2022-01-29 11:19:50 +0800

    Need improvement

--- remote-control-car/lib/remote_control_car.ex
+++ remote-control-car/lib/remote_control_car.ex
@@ -6,23 +6,30 @@ defmodule RemoteControlCar do
     distance_driven_in_meters: 0
   ]
 
-  def new() do
-    %RemoteControlCar{nickname: "none"}
-  end
-
-  def new(nickname) do
+  def new(nickname \\ "none") do
     %RemoteControlCar{nickname: nickname}
   end
 
-  def display_distance(remote_car) do
-    # Please implement the display_distance/1 function
+  def display_distance(remote_car) when is_struct(remote_car, RemoteControlCar) do
+    "#{remote_car.distance_driven_in_meters} meters"
   end
 
-  def display_battery(remote_car) do
-    # Please implement the display_battery/1 function
+  def display_battery(remote_car) when is_struct(remote_car, RemoteControlCar) do
+    pct = remote_car.battery_percentage
+    case pct do
+      0 -> "Battery empty"
+      _ -> "Battery at #{pct}%"
+    end
   end
 
-  def drive(remote_car) do
-    # Please implement the drive/1 function
+  def drive(remote_car) when is_struct(remote_car, RemoteControlCar) do
+    distance = remote_car.distance_driven_in_meters
+    battery  = remote_car.battery_percentage
+    if battery != 0 do
+      %{remote_car | distance_driven_in_meters: distance + 20, battery_percentage: battery - 1}
+    else
+      remote_car
+    end
   end
+
 end
