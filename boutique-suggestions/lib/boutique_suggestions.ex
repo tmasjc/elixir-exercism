71883f33a528ba38748767597b182f8e600e87ac
tmasjc <thomas.jcng@gmail.com>
2022-02-03 12:40:29 +0800

    Done solution
    working with list comprehension.

--- boutique-suggestions/lib/boutique_suggestions.ex
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,16 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
-    # Please implement the get_combinations/3 function
+  @opts_default maximum_price: 100
+
+  @spec get_combinations([map], [map], keyword) :: list
+  def get_combinations(tops, bottoms, options \\ []) do
+
+    options = Keyword.merge(@opts_default, options)
+
+    for x <- tops,
+        y <- bottoms,
+        x.base_color != y.base_color,
+        x.price + y.price <= options[:maximum_price] do
+      {x, y}
+    end
   end
 end
