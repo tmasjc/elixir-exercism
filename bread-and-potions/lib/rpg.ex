fbbf38c0237ae9293315ca854130e4272be89171
tmasjc <thomas.jcng@gmail.com>
2022-02-04 22:54:19 +0800

    Done solution
    working with protocols

--- bread-and-potions/lib/rpg.ex
+++ bread-and-potions/lib/rpg.ex
@@ -1,6 +1,6 @@
 defmodule RPG do
   defmodule Character do
-    defstruct health: 100, mana: 0
+   defstruct health: 100, mana: 0
   end
 
   defmodule LoafOfBread do
@@ -20,4 +20,19 @@ defmodule RPG do
   end
 
   # Add code to define the protocol and its implementations below here...
+  defprotocol Edible do
+    def eat(item, char)
+  end
+
+  defimpl RPG.Edible, for: LoafOfBread do
+    def eat(_, char), do: { nil, %{char | health: char.health + 5} }
+  end
+
+  defimpl RPG.Edible, for: ManaPotion do
+    def eat(potion, char), do: { %EmptyBottle{}, %{char | mana: char.mana + potion.strength} }
+  end
+
+  defimpl RPG.Edible, for: Poison do
+    def eat(_, char), do: { %EmptyBottle{}, %{char | health: 0} }
+  end
 end
